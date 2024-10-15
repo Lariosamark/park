f9fa39c8239c33681ce41dc541ced80270c5cc51
mrcky <80890822+Lariosamark@users.noreply.github.com>
2024-10-15 16:59:28 +0000

    ujten

--- src/pages/dashboard/PermitPage.jsx
+++ src/pages/dashboard/PermitPage.jsx
@@ -1,32 +1,41 @@
 import Permit from "../../components/dashboard/Permit";
+import { createNotification } from "../../lib/notification";
 import { updatePermit } from "../../lib/permit";
+import { useUser } from "../../providers/AuthProvider";
 import LoadingPage from "../LoadingPage";
 import { usePermit } from "./usePermit";
 import { useParams } from "react-router-dom";
 
 export default function PermitPage() {
   const { permitId } = useParams();
+  const { user } = useUser();
 
   const { permit, loading } = usePermit(permitId);
 
   if (loading) return <LoadingPage />;
   if (!permit) return <>No Permit</>;
 
+  const handleUpdate = async (status) => {
+    await updatePermit(permit.id, { status });
+    await createNotification(
+      user.id,
+      permit.userId,
+      `Admin ${status.toLowerCase()} your request`,
+      { haha: "haha" }
+    );
+  };
+
   return (
     <div>
       <Permit permitData={permit} />
       <button
         className="bg-green-900 text-white px-4 py-2 rounded-md"
-        onClick={async () =>
-          await updatePermit(permit.id, { status: "Approved" })
-        }
+        onClick={() => handleUpdate("Approved")}
       >
         Approve
       </button>
       <button
-        onClick={async () =>
-          await updatePermit(permit.id, { status: "Declined" })
-        }
+        onClick={() => handleUpdate("Declined")}
         className="bg-red-900 text-white px-4 py-2 rounded-md"
       >
         Decline
